c5c631d14e66e4f8191d240433c0123a3bf035dd
MichaelEight <werykintosz@wp.eu>
2023-10-10 01:06:53 +0200

    Early sketch of general GUI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-// import logo from './logo.svg';
-import logo from './Images/logoSUS.png';
+import Header from './WebsiteGeneralElements/Header.js'
+import Navbar from './WebsiteGeneralElements/Navbar.js'
 import React, { useState } from 'react';
 import './App.css';
+
+function MainContent() {
+  return (
+    <main>
+      {/* Main content goes here */}
+      <h1>Main Content goes here</h1>
+    </main>
+  );
+}
+
+function Sidebar() {
+  return (
+    <aside>
+      {/* Sidebar content goes here */}
+      <p>Sidebar Content</p>
+      <p>Option 1</p>
+      <p>Option 2</p>
+      <p>Option 3</p>
+    </aside>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Great things will appear here... hopefully!
-        </p>
-      </header>
+      <Header />
+      <Navbar />
+      <div className="container">
+        <Sidebar />
+        <MainContent />
+      </div>
     </div>
   );
 }
---
+++ src/WebsiteGeneralElements/Navbar.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import '../Navbar.css';
+
+export default function Navbar(){
+    return(
+        <nav className='navigation-menu'>
+            <ul>
+                <li><p>Link 1</p></li>
+                <li><p>Link 2</p></li>
+                <li><p>Link 3</p></li>
+            </ul>
+        </nav>
+    );
+}
