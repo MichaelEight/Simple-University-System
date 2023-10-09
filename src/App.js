08b07b098a314dd73ce4d1f3281417860ffc014f
MichaelEight <werykintosz@wp.eu>
2023-10-09 20:33:36 +0200

    Introduced example code in React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import RandomFunc from './ExampleFunc.js'
+
+function DisplayText()
+{
+  return(
+    <p>Some Text</p>
+  );
+}
+
+function NewTemplate() {
+  return (
+    <div>
+      {/* Your new template content */}
+      <h2>New Template</h2>
+      <p>This is the new content you want to display.</p>
+    </div>
+  );
+}
+
+function OldTemplate(){
+  return(
+    <p>Old Template</p>
+  );
+}
+
+function Template1() {
+  // Bool variable with "get" and "set" functions, set value to "false"
+  const [showNewTemplate, setShowNewTemplate] = useState(false);
+
+  // Function that inverts bool variable 
+  const toggleTemplate = () => {
+    setShowNewTemplate(!showNewTemplate);
+  };
+
+  return (
+    <div>
+      <h1>My React Site</h1>
+      <button onClick={toggleTemplate}>Toggle Template</button>
+      {showNewTemplate ? <NewTemplate /> : <OldTemplate />}
+    </div>
+  );
+}
 
 function App() {
+  const inputStyle = {
+    fontSize: '69px',
+    // You can add other CSS properties here if needed
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Great things will appear here... hopefully!
+          <i>Great</i> things will appear here... hopefully!🥵
         </p>
+        {/* <DisplayText />
+        <RandomFunc /> */}
+        <input type="text" style={inputStyle} placeholder="Type something here" />
+        <Template1 />
       </header>
     </div>
   );
---
+++ src/ExampleFunc.js
@@ -0,0 +1,15 @@
+function Foo()
+{
+    return(
+        <p>Apes Together Strong</p>
+    );
+}
+
+function Bar()
+{
+    return(
+        <h1>Leeeeeerooooooooy Jeeenkins</h1>
+    );
+}
+
+export default Foo;
