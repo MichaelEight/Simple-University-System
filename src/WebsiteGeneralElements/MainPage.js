62cd093e804fc677936f6449dd1f32e34c38f8af
MichaelEight <werykintosz@wp.eu>
2023-10-10 22:19:16 +0200

    Fixed formatting

--- src/WebsiteGeneralElements/MainPage.js
+++ src/WebsiteGeneralElements/MainPage.js
@@ -1,9 +1,8 @@
-import './Main.css'
+import './MainPage.css'
 
 function Sidebar() {
     return (
         <aside>
-        {/* Sidebar content goes here */}
         <p>Sidebar Content</p>
         <p>Option 1</p>
         <p>Option 2</p>
@@ -14,24 +13,18 @@ function Sidebar() {
     );
 }
 
-function Content() {
-    const textStyleCentered = {
-        display: 'flex',
-        flexDirection: 'column', // Stack text vertically
-        alignItems: 'center',     // Center horizontally
-      };
-
+function Content() {    
     return (
         <main>
-            
-        <main style={textStyleCentered}>
-        {/* Main content goes here */}
-        <p > Monotechnika Akademicka  </p>
-        <p style={{fontWeight: 'bold', fontSize: '32px'}} >WITAJ W SYSTEMIE SUS</p>
-        </main>
+        <div>
+            <div className='textStyleCentered'>
+                <p>Monotechnika Akademicka</p>
+                <p style={{fontWeight: 'bold', fontSize: '32px'}} >WITAJ W SYSTEMIE SUS</p>
+            </div>
 
-        <p style={{ fontWeight: 'bold', fontSize: '24px', margin: 10, borderBottom: '2px solid #999' }}>UWAGA TECHNICZNA!</p>
-        <p style={{margin: 0}}>Każdego dnia w godzinach zależny od fazy księżyca, pogody i wielu innych czynników niezależnych od nas - system może nie działać. Wszelkie skargi proszę składać do prorektora Politechniki Wrocławskiej Kamila Stańca.</p>
+            <p style={{ fontWeight: 'bold', fontSize: '24px', margin: 10, borderBottom: '2px solid #999' }}>UWAGA TECHNICZNA!</p>
+            <p style={{margin: 0}}>Każdego dnia w godzinach zależny od fazy księżyca, pogody i wielu innych czynników niezależnych od nas - system może nie działać. Wszelkie skargi proszę składać do prorektora Politechniki Wrocławskiej Kamila Stańca.</p>
+        </div>
         </main>
     );
 }
