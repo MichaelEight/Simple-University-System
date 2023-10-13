3086c6ccc3820af84d72198e7f5c876f518a490c
MichaelEight <werykintosz@wp.eu>
2023-10-13 22:21:03 +0200

    Example code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Main from './GeneralElements/Main.js'
 import Footer from './GeneralElements/Footer.js'
 import LoginBar from './GeneralElements/Loginbar.js'
 import './App.css';
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [selectedItem, setSelectedItem] = useState(0);
@@ -13,6 +14,19 @@ function App() {
     setSelectedItem(index);
   };
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Update the URL with your server's URL
+    axios.get('http://localhost:3001/api/data')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <LoginBar />
@@ -20,6 +34,15 @@ function App() {
       <Navbar selectedItem={selectedItem} onNavItemChange={handleNavItemChange} />
       <Main selectedItem={selectedItem} />
       <Footer />
+
+      <div>
+        <h1>Data from Database</h1>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+      </div>
     </div>
   );
 }
