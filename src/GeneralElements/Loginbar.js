14f10c355961fe8d2510020804e91a7f46d60b3c
MichaelEight <werykintosz@wp.eu>
2023-10-13 05:43:24 +0200

    Added cookies to keep user logged in

--- src/GeneralElements/Loginbar.js
+++ src/GeneralElements/Loginbar.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie'; // Import the js-cookie library
 import '../GeneralElementsStyles/Loginbar.css';
 import '../GlobalStyles.css';
 
@@ -9,24 +10,47 @@ export default function LoginBar() {
     const [password, setPassword] = useState('');
     const [student, setStudent] = useState(null);
     const [isValidEmail, setIsValidEmail] = useState(true);
+    const [keepLoggedIn, setKeepLoggedIn] = useState(false); // State for "Keep me logged in" checkbox
+  
+    useEffect(() => {
+      // Check if the user is logged in using cookies
+      const isLoggedIn = Cookies.get('loggedIn') === 'true';
+      if (isLoggedIn) {
+        // Retrieve user information from cookies and set the session
+        const studentInfo = JSON.parse(Cookies.get('studentInfo'));
+        setLoggedIn(true);
+        if (studentInfo) {
+          setStudent(studentInfo);
+        }
+      }
+    }, []); // Run this effect once on component mount
   
     const handleLogin = () => {
-      // Simulate login logic here
-      if (isValidEmail && email === 'user@example.com' && password === 'password') {
-        setLoggedIn(true);
-        setStudent({ id: 1, name: 'John', lastname: 'Doe' });
-        setShowLoginPopup(false);
-        // You should set a session token or cookie here
-      } else {
-        alert('Invalid email or password');
-      }
-    };
+        if (isValidEmail && email === 'user@example.com' && password === 'password') {
+          const studentInfo = { id: 1, name: 'John', lastname: 'Doe' }; // Example user data
+          console.log('Logging in with user data:', studentInfo); // Add this line for debugging
+          setLoggedIn(true);
+          setStudent(studentInfo);
+      
+          // Save session information in cookies if "Keep me logged in" is checked
+          if (keepLoggedIn) {
+            Cookies.set('loggedIn', 'true', { expires: 7 }); // Store the session for 7 days
+            Cookies.set('studentInfo', JSON.stringify(studentInfo), { expires: 7 });
+          }
+      
+          setShowLoginPopup(false);
+        } else {
+          alert('Invalid email or password');
+        }
+      };
   
     const handleLogout = () => {
-      // Simulate logout logic here
+      // Clear cookies and log out the user
+      Cookies.remove('loggedIn');
+      Cookies.remove('studentInfo');
+  
       setLoggedIn(false);
       setStudent(null);
-      // You should clear the session token or cookie here
     };
   
     const handleEmailChange = (event) => {
@@ -41,62 +65,75 @@ export default function LoginBar() {
     };
   
     const handleClosePopup = () => {
-        // Clear input fields and reset validation states
-        setEmail('');
-        setPassword('');
-        setIsValidEmail(true);
-        setShowLoginPopup(false);
+      // Clear input fields and reset validation states
+      setEmail('');
+      setPassword('');
+      setIsValidEmail(true);
+      setShowLoginPopup(false);
     };
-
+  
     return (
-        <div className="login-bar login-right global-shadow">
-          {!loggedIn ? (
-            <>
-              <div className="login-right login-bar-text">
-                <p>Not logged in</p>
-                <button onClick={() => setShowLoginPopup(true)}>Log in</button>
-              </div>
-            </>
-          ) : (
+      <div className="login-bar login-right global-shadow">
+        {!loggedIn ? (
+          <>
             <div className="login-right login-bar-text">
+              <p>Not logged in</p>
+              <button onClick={() => setShowLoginPopup(true)}>Log in</button>
+            </div>
+          </>
+        ) : (
+          <div className="login-right login-bar-text">
+            {student ? (
               <p>Logged as {student.id}, {student.name}, {student.lastname}</p>
-              <button onClick={handleLogout}>Log out</button>
-            </div>
-          )}
-          {showLoginPopup && (
-            <div className="login-popup-overlay">
-              <div className="login-popup">
-                <span className="close" onClick={handleClosePopup}>
-                  &times;
-                </span>
-                <h2>Login</h2>
-                <div className="login-input">
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="text"
-                    id="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    onBlur={handleEmailBlur}
-                    className={isValidEmail ? '' : 'invalid-email'} // Apply 'invalid-email' class for invalid emails
-                  />
-                  {!isValidEmail && <p className="error-message">Invalid email</p>}
-                </div>
-                <div className="login-input">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <button onClick={handleLogin}>Login</button>
+            ) : (
+              <p>Logged in</p>
+            )}
+            <button onClick={handleLogout}>Log out</button>
+          </div>
+        )}
+        {showLoginPopup && (
+          <div className="login-popup-overlay">
+            <div className="login-popup">
+              <span className="close" onClick={handleClosePopup}>
+                &times;
+              </span>
+              <h2>Login</h2>
+              <div className="login-input">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  onBlur={handleEmailBlur}
+                  className={isValidEmail ? '' : 'invalid-email'} // Apply 'invalid-email' class for invalid emails
+                />
+                {!isValidEmail && <p className="error-message">Invalid email</p>}
               </div>
+              <div className="login-input">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="login-input">
+                <label htmlFor="keepLoggedIn">Keep me logged in</label>
+                <input
+                  type="checkbox"
+                  id="keepLoggedIn"
+                  checked={keepLoggedIn}
+                  onChange={() => setKeepLoggedIn(!keepLoggedIn)}
+                />
+              </div>
+              <button onClick={handleLogin}>Login</button>
             </div>
-          )}
-        </div>
-      );
-}
+          </div>
+        )}
+      </div>
+    );
+  }
