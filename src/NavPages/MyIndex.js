59127a004de00aaa7b6b4a2a189eba08d70f44bb
MichaelEight <werykintosz@wp.eu>
2023-10-19 17:05:16 +0200

    Stats Page Template

--- src/NavPages/MyIndex.js
+++ src/NavPages/MyIndex.js
@@ -1,6 +1,7 @@
 import '../NavPagesStyles/Sidebar.css';
 import '../NavPagesStyles/MyIndex.css';
 import '../NavPagesStyles/MyProfile.css';
+import '../NavPagesStyles/Stats.css';
 import React, { useState } from 'react';
 import placeholderPic from '../Images/placeholderImage.jpeg';
 
@@ -90,16 +91,21 @@ function ContentGrades() {
               <th>Prowadzący</th>
               <th>Ocena</th>
               <th>ECTS</th>
+              <th>Akcja</th>
             </tr>
           </thead>
           <tbody>
             {gradeData.map((item, index) => (
-              <tr key={index}>
+              <tr key={index} className='rows-colors'>
                 <td>{item.subjectCode}</td>
                 <td>{item.subjectName}</td>
                 <td>{item.instructorName}</td>
                 <td>{item.grade}</td>
                 <td>{item.ects}</td>
+                <td>
+                  <button>Zaakceptuj</button>
+                  <button>Reklamuj</button>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -110,10 +116,33 @@ function ContentGrades() {
 }
 
 function ContentStats() {
+  // Placeholder data for the student's stats
+  const studentStats = {
+    name: 'John Doe',
+    grades: [85, 92, 78, 96, 89],
+    totalStudents: 50, // Total number of students
+  };
+
+  // Calculate the student's position and percentile
+  const position = 5; // Student's rank
+  const percentile = ((studentStats.totalStudents - position) / studentStats.totalStudents) * 100;
+
   return (
     <main>
-      <div>
-        <p>Placeholder Stats</p>
+      <div className="content-stats">
+        <h2>{studentStats.name}'s Statistics</h2>
+        <div className="grade-summary">
+          <p>Your grades:</p>
+          <ul>
+            {studentStats.grades.map((grade, index) => (
+              <li key={index}>Exam {index + 1}: {grade}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="position-percentile">
+          <p>You are in the top {position} students based on grades.</p>
+          <p>Your grades are better than {percentile.toFixed(2)}% of other students.</p>
+        </div>
       </div>
     </main>
   );
