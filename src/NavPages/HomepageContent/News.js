533da859ec3626a5a9de534ca93d1d35e5f9af51
MichaelEight <werykintosz@wp.eu>
2023-12-16 17:05:22 +0100

    small visual fixes on homepage

--- src/NavPages/HomepageContent/News.js
+++ src/NavPages/HomepageContent/News.js
@@ -4,8 +4,10 @@ export default function ContentNews() {
       <div>
         <div className='main-center-text border-box'>
           <p><b>Monotechnika Akademicka</b></p>
-          <p style={{ fontWeight: 'bold', fontSize: '32px' }}>WITAJ W SYSTEMIE SUS</p>
+          <p style={{ fontWeight: 'bold', fontSize: '32px', marginTop:'10px', marginBottom:'54px' }}>WITAJ W SYSTEMIE SUS</p>
         </div>
+        
+        <div className="main-spacer"></div>
 
         <p style={{ fontWeight: 'bold', fontSize: '24px', margin: 10, borderBottom: '2px solid #999' }}>UWAGA TECHNICZNA</p>
         <p style={{ margin: 0, textAlign: 'justify' }}>Każdego dnia w godzinach zależnych od fazy księżyca, pogody i wielu innych czynników niezależnych od nas - system może nie działać. Wszelkie skargi proszę składać do prorektora naszej Monotechniki, który z radością podejmie się złożonej analizy problemu. W ciągu od 9 do ∞ dni roboczych proszę oczekiwać odpowiedzi negatywnej.</p>
