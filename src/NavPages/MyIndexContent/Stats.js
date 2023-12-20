0fa946c1dbe8e8362a7039dd6792d8110fbde42f
MichaelEight <werykintosz@wp.eu>
2023-12-20 01:18:56 +0100

    New placeholder for stats, simplified

--- src/NavPages/MyIndexContent/Stats.js
+++ src/NavPages/MyIndexContent/Stats.js
@@ -1,30 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import '../../NavPagesStyles/Stats.css';
 
-export default function ContentStats({user}) {
-    // Placeholder data for the student's stats
-    const studentStats = {
-      name: 'John Doe',
-      grades: [85, 92, 78, 96, 89],
-      totalStudents: 50, // Total number of students
-    };
-  
+export default function ContentStats({user}) {  
     // Calculate the student's position and percentile
-    const position = 5; // Student's rank
-    const percentile = ((studentStats.totalStudents - position) / studentStats.totalStudents) * 100;
+    const position = 10; // Student's rank
+    const percentile = 90.00;
   
     return (
       <main>
         <div className="content-stats">
-          <h2>{studentStats.name}'s Statistics</h2>
-          <div className="grade-summary">
-            <p>Your grades:</p>
-            <ul>
-              {studentStats.grades.map((grade, index) => (
-                <li key={index}>Exam {index + 1}: {grade}</li>
-              ))}
-            </ul>
-          </div>
+          <h2>{user.name} {user.lastname}'s Statistics</h2>
           <div className="position-percentile">
             <p>You are in the top {position} students based on grades.</p>
             <p>Your grades are better than {percentile.toFixed(2)}% of other students.</p>
