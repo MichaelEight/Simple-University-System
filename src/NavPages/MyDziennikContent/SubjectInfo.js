8185e31c4005158d767055caa44ed8958cd8d4f3
MichaelEight <werykintosz@wp.eu>
2023-12-25 15:16:58 +0100

    MyDziennik view your subjects and edit some info about it

--- src/NavPages/MyDziennikContent/SubjectInfo.js
+++ src/NavPages/MyDziennikContent/SubjectInfo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './SubjectInfo.css';
 
 export default function ContentSubjectInfo({ user }) {
   const [subjects, setSubjects] = useState([]);
@@ -7,6 +8,9 @@ export default function ContentSubjectInfo({ user }) {
   const [updatedLiterature, setUpdatedLiterature] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
 
   const fetchSubjects = async () => {
       setIsLoading(true);
@@ -32,19 +36,43 @@ export default function ContentSubjectInfo({ user }) {
       fetchSubjects();
   }, []);
 
-    const handleSubjectSelect = (subject) => {
-        setSelectedSubject(subject);
-        setUpdatedConsultations(subject.konsultacje);
-        setUpdatedLiterature(subject.literatura);
-    };
+  const handleSubjectSelect = (subjectId) => {
+    const selected = subjects.find(sub => sub.subject_id === subjectId);
+    if (selected) {
+        setSelectedSubject(selected);
+        setUpdatedConsultations(selected.konsultacje);
+        setUpdatedLiterature(selected.literatura);
+        setMessage('');
+        setMessageType('');
+    }
+  };
+
 
     const handleSubmitChanges = async () => {
         try {
             const response = await fetch(`https://simpleuniversitysystem.000webhostapp.com/api/updateSubjectDetails.php?token=${user.token}&subject_id=${selectedSubject.subject_id}&konsultacje=${updatedConsultations}&literatura=${updatedLiterature}`);
             if (response.ok) {
                 console.log("Changes successfully saved");
+                setMessage('Zmiany wprowadzono pomyślnie!');
+                setMessageType('success');
+
+                // Update the local data
+                // Assuming 'subjects' is the state holding the list of subjects
+                const updatedSubjects = subjects.map(subject => {
+                    if (subject === selectedSubject) {
+                        return {
+                            ...subject,
+                            konsultacje: updatedConsultations, // Update with the current value from the state
+                            literatura: updatedLiterature   // Update with the current value from the state
+                        };
+                    }
+                    return subject;
+                });
+                setSubjects(updatedSubjects);
             } else {
                 console.error("Failed to save changes");
+                setMessage('Wystąpił problem podczas wprowadzania zmian!');
+                setMessageType('error');
             }
         } catch (error) {
             console.error("Error saving changes", error);
@@ -52,34 +80,60 @@ export default function ContentSubjectInfo({ user }) {
     };
 
     return (
-      <main>
-          {isLoading ? (
-              <p>Trwa ładowanie, proszę czekać...</p>
-          ) : error ? (
-              <p>{error}</p>
-          ) : (
-              <div>
-                  <div style={{ height: '300px', overflowY: 'scroll' }}>
-                      {subjects.map(subject => (
-                          <div key={subject.subject_id} onClick={() => handleSubjectSelect(subject)}>
-                              {subject.subject_code} - {subject.subject_name}
-                          </div>
-                      ))}
-                  </div>
-                  {selectedSubject && (
-                    <div>
-                      <p>{selectedSubject.subject_name}</p>
-                      <p>{selectedSubject.subject_code}</p>
-                      <p>{selectedSubject.ects}</p>
-                      <p>{selectedSubject.wydział}</p>
-                      <p>{selectedSubject.sylabus}</p>
-                      <input type="text" value={updatedConsultations} onChange={e => setUpdatedConsultations(e.target.value)} />
-                      <input type="text" value={updatedLiterature} onChange={e => setUpdatedLiterature(e.target.value)} />
-                      <button onClick={handleSubmitChanges}>Zatwierdź zmiany</button>
-                    </div>
-                  )}
-              </div>
-          )}
-      </main>
+        <main>
+            {isLoading ? (
+                <p>Trwa ładowanie, proszę czekać...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
+                <div className="subject-info-container">
+                        <div className="subject-list">
+                        {subjects.map(subject => (
+                            <div
+                                key={subject.subject_id}
+                                className={`subject-item ${selectedSubject && subject.subject_id === selectedSubject.subject_id ? 'selectedSubject' : ''}`}
+                                onClick={() => handleSubjectSelect(subject.subject_id)}
+                            >
+                                {subject.subject_code} - {subject.subject_name}
+                            </div>
+                        ))}
+                </div>
+                    {selectedSubject && (
+                        <div className="subject-details">
+                            <h2>Szczegóły Przedmiotu</h2>
+                            <p><strong>Nazwa przedmiotu:</strong> {selectedSubject.subject_name}</p>
+                            <p><strong>Kod przedmiotu:</strong> {selectedSubject.subject_code}</p>
+                            <p><strong>Liczba punktów ECTS:</strong> {selectedSubject.ects}</p>
+                            <p><strong>Kierunek:</strong> {selectedSubject.wydział}</p>
+                            <p><strong>Sylabus:</strong> {selectedSubject.sylabus}</p>
+                            <div className="editable-field">
+                                <label htmlFor="konsultacje">Konsultacje:</label>
+                                <input
+                                    type="text"
+                                    id="konsultacje"
+                                    value={updatedConsultations}
+                                    onChange={e => setUpdatedConsultations(e.target.value)} />
+                            </div>
+                            <div className="editable-field">
+                                <label htmlFor="literatura">Literatura:</label>
+                                <input
+                                    type="text"
+                                    id="literatura"
+                                    value={updatedLiterature}
+                                    onChange={e => setUpdatedLiterature(e.target.value)} />
+                            </div>
+                            <div className="submit-button-container">
+                                <button onClick={handleSubmitChanges}>Zatwierdź zmiany</button>
+                            </div>
+                            {message && (
+                                <div className={`message ${messageType}`}>
+                                    {message}
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
+            )}
+        </main>
     );
 }
